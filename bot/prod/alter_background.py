7001bf3bfb184f2440a6d639cc8b2627916cacdc
nico <nicolasvo95@gmail.com>
2022-02-04 14:45:31 +0100

    add interpolation nearest to resize

--- bot/prod/alter_background.py
+++ bot/prod/alter_background.py
@@ -1,6 +1,8 @@
 import numpy as np
 import cv2
+from PIL import Image
 from pixellib.tune_bg import alter_bg
+from image import rescale_img, write_img
 
 
 class AlterBackground(alter_bg):
@@ -12,8 +14,8 @@ class AlterBackground(alter_bg):
         )
 
         ori_img = cv2.imread(img_path, cv2.IMREAD_UNCHANGED)
-        scaled_img = self.rescale_img(ori_img)
-        self.write_img(scaled_img, scaled_img_path)
+        scaled_img = rescale_img(ori_img)
+        write_img(scaled_img, scaled_img_path)
 
         seg_image = self.segmentAsPascalvoc(scaled_img_path)
 
@@ -42,26 +44,79 @@ class AlterBackground(alter_bg):
 
         return out
 
-    def rescale_img(self, img):
-        height, width, _ = img.shape
-        if [height, width].index(max([height, width])) == 0:
-            factor = 512 / height
-            height = 512
-            width = int(width * factor)
+    def segmentAsPascalvoc(self, image_path, process_frame=False):
+        if process_frame == True:
+            image = image_path
         else:
-            factor = 512 / width
-            width = 512
-            height = int(height * factor)
+            image = cv2.imread(image_path)
 
-        img = cv2.resize(
-            img, dsize=[width, height], dst=factor, interpolation=cv2.INTER_LINEAR
+        h, w, n = image.shape
+
+        if n > 3:
+            image = cv2.cvtColor(image, cv2.COLOR_BGRA2BGR)
+        resize_ratio = 1.0 * self.INPUT_SIZE / max(w, h)
+        target_size = (int(resize_ratio * w), int(resize_ratio * h))
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        resized_image = cv2.resize(image, target_size, interpolation=cv2.INTER_AREA)
+        batch_seg_map = self.sess.run(
+            self.OUTPUT_TENSOR_NAME,
+            feed_dict={self.INPUT_TENSOR_NAME: [np.asarray(resized_image)]},
         )
 
-        return img
+        seg_image = batch_seg_map[0]
+        raw_labels = seg_image
+        labels = self.obtain_segmentation(seg_image)
+        labels = np.array(
+            Image.fromarray(labels.astype("uint8")).resize(
+                (w, h), resample=Image.NEAREST
+            )
+        )
+        labels = cv2.cvtColor(labels, cv2.COLOR_RGB2BGR)
 
-    def write_img(self, img, out_img_path):
-        cv2.imwrite(out_img_path, img)
+        return raw_labels, labels
+
+    def obtain_segmentation(self, image, nc=21):
+        colors = self.label_pascal()
+        r = np.zeros_like(image).astype(np.uint8)
+        g = np.zeros_like(image).astype(np.uint8)
+        b = np.zeros_like(image).astype(np.uint8)
+
+        for a in range(0, nc):
+            index = image == a
+            r[index] = colors[a, 0]
+            g[index] = colors[a, 1]
+            b[index] = colors[a, 2]
+            rgb = np.stack([r, g, b], axis=2)
+
+        return rgb
+
+    def label_pascal(self):
+        return np.asarray(
+            [
+                [0, 0, 0],
+                [128, 0, 0],
+                [0, 128, 0],
+                [255, 255, 255],
+                [0, 0, 128],
+                [128, 0, 128],
+                [0, 128, 128],
+                [128, 128, 128],
+                [64, 0, 0],
+                [192, 0, 0],
+                [64, 128, 0],
+                [192, 128, 0],
+                [64, 0, 128],
+                [192, 0, 128],
+                [64, 128, 128],
+                [255, 255, 255],
+                [0, 64, 0],
+                [128, 64, 0],
+                [0, 192, 0],
+                [128, 192, 0],
+                [0, 64, 128],
+            ]
+        )
 
     def boom(self, img_path, out_img_path):
         img = self.remove_bg(img_path)
-        self.write_img(img, out_img_path)
+        write_img(img, out_img_path)
