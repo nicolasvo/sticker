79a55234bbdc04fa59cfbedde906c5d29758af75
nico <nicolasvo95@gmail.com>
2022-02-04 14:44:48 +0100

    move rescale and write img functions

---
+++ bot/prod/image.py
@@ -0,0 +1,23 @@
+import cv2
+
+
+def rescale_img(img):
+    height, width, _ = img.shape
+    if [height, width].index(max([height, width])) == 0:
+        factor = 512 / height
+        height = 512
+        width = int(width * factor)
+    else:
+        factor = 512 / width
+        width = 512
+        height = int(height * factor)
+
+    img = cv2.resize(
+        img, dsize=[width, height], dst=factor, interpolation=cv2.INTER_LINEAR
+    )
+
+    return img
+
+
+def write_img(img, out_img_path):
+    cv2.imwrite(out_img_path, img)
