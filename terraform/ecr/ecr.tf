e40f05427948f6a5e73e97bef46b7338cdbf1efe
nico <nicolasvo95@gmail.com>
2022-11-06 13:23:49 +0100

    add lambda producer

--- terraform/ecr/ecr.tf
+++ terraform/ecr/ecr.tf
@@ -1,3 +1,12 @@
+resource "aws_ecr_repository" "producer" {
+  name                 = "sticker-bot-producer"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
 resource "aws_ecr_repository" "bot" {
   name                 = "sticker-bot"
   image_tag_mutability = "MUTABLE"
