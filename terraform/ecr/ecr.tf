6cce646946161241b4d9533dbd9277c30a414cc9
nico <nicolasvo95@gmail.com>
2022-11-06 13:44:08 +0100

    update ecr

--- terraform/ecr/ecr.tf
+++ terraform/ecr/ecr.tf
@@ -1,6 +1,6 @@
-resource "aws_ecr_repository" "producer" {
-  name                 = "sticker-bot-producer"
-  image_tag_mutability = "MUTABLE"
+resource "aws_ecr_repository" "bot_producer" {
+  name                 = "sticker-producer"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
@@ -8,8 +8,8 @@ resource "aws_ecr_repository" "producer" {
 }
 
 resource "aws_ecr_repository" "bot" {
-  name                 = "sticker-bot"
-  image_tag_mutability = "MUTABLE"
+  name                 = "sticker"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
@@ -17,8 +17,8 @@ resource "aws_ecr_repository" "bot" {
 }
 
 resource "aws_ecr_repository" "bot_down" {
-  name                 = "sticker-bot-down"
-  image_tag_mutability = "MUTABLE"
+  name                 = "sticker-down"
+  image_tag_mutability = "IMMUTABLE"
 
   image_scanning_configuration {
     scan_on_push = true
