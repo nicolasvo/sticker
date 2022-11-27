2c4daab4d4a7515f30fcb9cb98eca4f57c905fd9
nico <nicolasvo95@gmail.com>
2022-11-27 10:56:50 +0100

    enable alpha matting

--- lambda/bot/models.py
+++ lambda/bot/models.py
@@ -9,7 +9,7 @@ def segment_u2net(img_path, out_path):
     with open(img_path, "rb") as i:
         with open(out_path, "wb") as o:
             input = i.read()
-            output = remove(input)
+            output = remove(input, alpha_matting=True, alpha_matting_erode_size=0)
             o.write(output)
     img = cv2.imread(out_path, cv2.IMREAD_UNCHANGED)
     img = add_outline(img, threshold=0, stroke_size=6, colors=((255, 255, 255),))
