2f35beb088820a23bd70d0b6368605fbbd7aa93b
nico <nicolasvo95@gmail.com>
2022-11-30 19:36:43 +0100

    change message

--- lambda/producer/app.py
+++ lambda/producer/app.py
@@ -22,12 +22,15 @@ def handler(event, context):
     body = json.loads(event["body"])
     if body.get("callback_query"):
         message_id = body["callback_query"]["message"]["message_id"]
-        reply_message_id = body["callback_query"]["message"]["reply_to_message"]["message_id"]
+        reply_message_id = body["callback_query"]["message"]["reply_to_message"][
+            "message_id"
+        ]
         chat_id = body["callback_query"]["message"]["chat"]["id"]
+        reply_data = body["callback_query"]["data"]
         bot.edit_message_text(
             message_id=message_id,
             chat_id=chat_id,
-            text=None,
+            text=f"You chose {emoji_number(int(reply_data))}",
             reply_markup=None,
         )
         bot.send_message(
