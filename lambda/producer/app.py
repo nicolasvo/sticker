92570c55a21dbb94025227352881a4a9deb45fd6
nico <nicolasvo95@gmail.com>
2022-11-30 09:37:04 +0100

    add bot from producer

--- lambda/producer/app.py
+++ lambda/producer/app.py
@@ -1,6 +1,14 @@
-import os
 import json
+import os
+import random
+
 import boto3
+from telegram import Bot
+
+from emojis import emoji_number, get_animated_emojis
+
+BOT_API_TOKEN = os.getenv("BOT_API_TOKEN")
+bot = Bot(BOT_API_TOKEN)
 
 
 def handler(event, context):
@@ -11,5 +19,23 @@ def handler(event, context):
     update = json.dumps(event["body"])
     response = queue.send_message(MessageBody=update)
     print(response)
+    print(event["body"])
+    print(type(event["body"]))
+    body = json.loads(event["body"])
+    if body.get("callback_query"):
+        message_id = body["callback_query"]["message"]["message_id"]
+        chat_id = body["callback_query"]["message"]["chat"]["id"]
+        reply_data = body["callback_query"]["data"]
+        bot.edit_message_text(
+            message_id=message_id,
+            chat_id=chat_id,
+            text=f"{emoji_number(int(reply_data))}",
+            reply_markup=None,
+        )
+        bot.send_message(
+            chat_id,
+            f"{random.choice(get_animated_emojis())}",
+            reply_to_message_id=message_id,
+        )
 
     return 200
