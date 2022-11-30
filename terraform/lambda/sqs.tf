085f051cd82cd817b29e27f116f6a5c1c1004d15
nico <nicolasvo95@gmail.com>
2022-11-30 21:39:52 +0100

    increase message size

--- terraform/lambda/sqs.tf
+++ terraform/lambda/sqs.tf
@@ -1,6 +1,6 @@
 resource "aws_sqs_queue" "queue" {
   name                       = var.queue_name
-  max_message_size           = 2048
+  max_message_size           = 4096
   message_retention_seconds  = 300
   visibility_timeout_seconds = 600
 }
