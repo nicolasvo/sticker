ab53bbc70d5b7f4b3d29e49fd043d6f13f17c5d5
nico <nicolasvo95@gmail.com>
2023-07-19 07:59:42 +0700

    Revert "async await what"
    This reverts commit 320d7a1fb6f6f5144d739f2e02a4c59b89ad19be.

--- lambda/bot/app.py
+++ lambda/bot/app.py
@@ -14,7 +14,7 @@ bot = Bot(BOT_API_TOKEN)
 dynamodb = boto3.resource('dynamodb')
 
 
-async def handler(event, context):
+def handler(event, context):
     try:
         _update = json.loads(json.loads(event["Records"][0]["body"]))
         update = Update.de_json(_update, bot)
@@ -44,7 +44,7 @@ async def handler(event, context):
             elif update.message.text and item and item.get("FileId"):
                 print("User sent a new prompt")
             else:
-                await bot.send_message(
+                bot.send_message(
                     update.message.chat_id,
                     f"Send me a picture! {random.choice(get_emojis())}",
                 )
