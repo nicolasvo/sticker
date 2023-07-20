6012b56435386905d65d643e7a7929d4e7502534
nico <nicolasvo95@gmail.com>
2023-07-21 03:46:44 +0700

    handle callback query

--- lambda/dev/hello_world/lambda_function.py
+++ lambda/dev/hello_world/lambda_function.py
@@ -5,7 +5,7 @@ import os
 from telegram import Bot, Update
 
 from dynamodb import get_item, upsert_item
-from sticker import segment_photo2
+from sticker import segment_photo2, make_sticker2
 
 loop = asyncio.get_event_loop()
 
@@ -14,34 +14,43 @@ async def main(event, context):
     try:
         BOT_API_TOKEN = os.getenv("BOT_API_TOKEN")
         bot = Bot(BOT_API_TOKEN)
+        print(f"event: {event}")
+        print(f"body: {event['body']}")
         update = json.loads(event["body"])
         update = Update.de_json(update, bot)
         print(update)
 
-        user_id = update.effective_user.id
-        item = get_item(user_id)
+        if not update.callback_query:
+            user_id = update.effective_user.id
+            item = get_item(user_id)
 
-        # delete
-        # send the sticker you want to delete
+            # delete
+            # send the sticker you want to delete
 
-        # new photo
-        if update.message.photo or update.message.document:
-            print("User sent a new photo")
-            upsert_item(
-                user_id, update.message.id, update.message.photo[-1].file_id, None
-            )
+            # new photo
+            if update.message.photo or update.message.document:
+                print("User sent a new photo")
+                upsert_item(
+                    user_id, update.message.id, update.message.photo[-1].file_id, None
+                )
 
-        # new prompt
-        elif update.message.text and item and item.get("FileId"):
-            print(f"User sent a new prompt: {update.message.text}")
-            await segment_photo2(update)
+            # new prompt
+            elif update.message.text and item and item.get("FileId"):
+                print(f"User sent a new prompt: {update.message.text}")
+                await segment_photo2(update)
 
-        # anything else
+            # anything else
+            else:
+                await bot.send_message(
+                    update.message.chat_id,
+                    f"Send me a picture la!",
+                )
         else:
-            await bot.send_message(
-                update.message.chat_id,
-                f"Send me a picture la!",
-            )
+            # user confirms sticker
+            answer = update.callback_query.data
+            if answer == "yes":
+                print("User confirmed sticker creation")
+                make_sticker2(update)
 
     except Exception as e:
         print(e)
--- lambda/dev/hello_world/sticker.py
+++ lambda/dev/hello_world/sticker.py
@@ -14,7 +14,7 @@ from telegram.ext import (
     CallbackContext,
 )
 
-from dynamodb import get_item
+from dynamodb import get_item, delete_item, upsert_item
 
 
 async def segment_photo2(update: Update) -> None:
@@ -32,8 +32,49 @@ async def segment_photo2(update: Update) -> None:
         photo_file = await bot.get_file(photo_id)
         await photo_file.download_to_drive(custom_path=photo_path)
         print("Photo downloaded")
+
+    keyboard = [
+        [
+            InlineKeyboardButton("yes", callback_data="yes"),
+            InlineKeyboardButton("no", callback_data="no"),
+        ]
+    ]
+    reply_markup = InlineKeyboardMarkup(keyboard)
+
+    # print("Storing base64 image into table item")
+    # upsert_item(user_id, segmented_photo=image_string)
+
     await bot.send_message(
         chat_id=update.effective_chat.id,
         reply_to_message_id=str(message_id),
+        reply_markup=reply_markup,
         text="Segmented this photo",
     )
+
+
+async def make_sticker2(update: Update) -> None:
+    # BOT_API_TOKEN = os.getenv("BOT_API_TOKEN")
+    # bot = Bot(BOT_API_TOKEN)
+    # await bot.initialize()
+    print("Making sticker")
+    item = get_item(update.effective_user.id)
+    # segmented_photo = item["SegmentedPhoto"]
+    with tempfile.TemporaryDirectory(dir="/tmp/") as tmpdirname:
+        output_path = f"{tmpdirname}/output.png"
+        # base64_to_image(segmented_photo, output_path)
+
+
+def image_to_base64(image_path):
+    import base64
+
+    with open(image_path, "rb") as image_file:
+        encoded_image = base64.b64encode(image_file.read())
+        return encoded_image.decode("utf-8")
+
+
+def base64_to_image(base64_string, output_file_path):
+    import base64
+
+    with open(output_file_path, "wb") as image_file:
+        decoded_image = base64.b64decode(base64_string)
+        image_file.write(decoded_image)
