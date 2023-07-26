1f37f4c6b9d303dd1ea2a11c18016c710f0246c5
nico <nicolasvo95@gmail.com>
2023-07-26 16:31:16 +0700

    copy weights from bucket

--- lambda/segment-sam/lambda_function.py
+++ lambda/segment-sam/lambda_function.py
@@ -1,6 +1,9 @@
 import base64
 import json
 import logging
+import os
+
+import boto3
 
 from segment import segment
 
@@ -8,6 +11,29 @@ logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 image_path = "/tmp/input.jpeg"
 output_path = "/tmp/output.png"
+bucket = os.getenv("BUCKET")
+bucket_folder = os.getenv("BUCKET_FOLDER")
+
+
+def copy_s3_folder_to_lambda(s3_bucket, s3_folder_key):
+    print("Copying weights folder")
+    # Create a Boto3 client for S3
+    s3_client = boto3.client("s3")
+
+    # List all objects in the given S3 folder
+    response = s3_client.list_objects_v2(Bucket=s3_bucket, Prefix=s3_folder_key)
+
+    # Check if the folder exists and has objects
+    if "Contents" in response:
+        for item in response["Contents"]:
+            # Get the object's key (filename)
+            object_key = item["Key"]
+
+            # Create a local file path within /tmp directory for the object
+            local_file_path = os.path.join("/tmp", os.path.basename(object_key))
+
+            # Download the object to the local file path
+            s3_client.download_file(s3_bucket, object_key, local_file_path)
 
 
 def image_to_base64(image_path):
@@ -24,6 +50,7 @@ def base64_to_image(base64_string, output_file_path):
 
 def lambda_handler(event, context):
     try:
+        copy_s3_folder_to_lambda(bucket, bucket_folder)
         body = json.loads(event["body"])
         image = body["image"]
         text_prompt = body["text_prompt"]
